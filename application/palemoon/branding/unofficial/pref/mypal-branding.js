772960d5de24afe47f8abc44b8d0039d03958c29
Fedor <fedor@mail.ru>
2019-03-12 20:36:59 +0300

    More mypal branding

---
+++ application/palemoon/branding/unofficial/pref/mypal-branding.js
@@ -0,0 +1,16 @@
+#filter substitution
+#filter emptyLines
+#include ../../shared/pref/preferences.inc
+#include ../../shared/pref/uaoverrides.inc
+
+pref("startup.homepage_override_url","https://feodor2.github.io/Mypal/");
+pref("app.releaseNotesURL", "https://github.com/Feodor2/Mypal/releases");
+
+// Enable Firefox compatmode by default.
+pref("general.useragent.compatMode", 2);
+pref("general.useragent.compatMode.gecko", true);
+pref("general.useragent.compatMode.firefox", true);
+
+// Updates disabled
+pref("app.update.enabled", false);
+pref("app.update.url", "");
