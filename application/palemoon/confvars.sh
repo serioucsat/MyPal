f67693f13ed113fc2fbc38ed906068ee86e93df1
Fedor <fedor@mail.ru>
2020-07-16 04:16:35 +0300

    [Mypal] Switch security storage to SQL.

--- application/palemoon/confvars.sh
+++ application/palemoon/confvars.sh
@@ -46,6 +46,9 @@ MOZ_PHOENIX=1
 # Lightweight Themes
 MOZ_PERSONAS=1
 
+# Enable SQL storage format for certs and passwords in NSS
+MOZ_SECURITY_SQLSTORE=1
+
 # Browser Feature: Profile Migration Component
 MOZ_PROFILE_MIGRATOR=
 
