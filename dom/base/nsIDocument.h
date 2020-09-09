d73c6ecc6a82c20f0cb14d02f07e284a16ca3fd6
Fedor <fedor@mail.ru>
2020-09-09 17:29:41 +0300

    Next part 2 of the Implement module type scripting.

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -2830,6 +2830,8 @@ public:
   virtual void ScheduleIntersectionObserverNotification() = 0;
   virtual void NotifyIntersectionObservers() = 0;
 
+  bool ModuleScriptsEnabled();
+
   bool ShouldThrowOnDynamicMarkupInsertion()
   {
     return mThrowOnDynamicMarkupInsertionCounter;
