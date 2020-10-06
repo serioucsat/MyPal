13230fb69d2809525772fb60854c467884e9f9b0
Fedor <fedor@mail.ru>
2020-10-06 18:21:34 +0300

    fix crash of ffmpeg

--- media/ffvpx/libavutil/log.c
+++ media/ffvpx/libavutil/log.c
@@ -361,7 +361,7 @@ void av_log_default_callback(void* ptr, int level, const char* fmt, va_list vl)
 
     if (level > av_log_level)
         return;
-    ff_mutex_lock(&mutex);
+    //ff_mutex_lock(&mutex);
 
     format_line(ptr, level, fmt, vl, part, &print_prefix, type);
     snprintf(line, sizeof(line), "%s%s%s%s", part[0].str, part[1].str, part[2].str, part[3].str);
@@ -398,7 +398,7 @@ void av_log_default_callback(void* ptr, int level, const char* fmt, va_list vl)
 #endif
 end:
     av_bprint_finalize(part+3, NULL);
-    ff_mutex_unlock(&mutex);
+    //ff_mutex_unlock(&mutex);
 }
 
 static void (*av_log_callback)(void*, int, const char*, va_list) =
