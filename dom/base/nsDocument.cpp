688f9fe3339585f2eef04eeae4c6588d5243a93e
Fedor <Fedor@mail.ru@>
2021-01-18 09:38:25 +0200

    [TB] Bug 12827: Create preference to disable SVG.

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -1272,6 +1272,7 @@ nsIDocument::nsIDocument()
     mGetUserFontSetCalled(false),
     mPostedFlushUserFontSet(false),
     mDidFireDOMContentLoaded(true),
+    mSVGStatus(mozilla::dom::SVGStatus_Unknown),
     mHasScrollLinkedEffect(false),
     mFrameRequestCallbacksScheduled(false),
     mBidiOptions(IBMBIDI_DEFAULT_BIDI_OPTIONS),
@@ -2033,6 +2034,8 @@ nsDocument::ResetToURI(nsIURI *aURI, nsILoadGroup *aLoadGroup,
   mReferrer.Truncate();
 
   mXMLDeclarationBits = 0;
+
+  mSVGStatus = SVGStatus_Unknown;
 
   // Now get our new principal
   if (aPrincipal) {
